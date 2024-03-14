34f3f88828fa6aa4fc8560bf6de98ae4776f6f70
rushikesh052 <rushikeshshelke052@gmail.com>
2024-03-14 18:48:02 +0530

    var added

--- src/main/java/com/rushi/SbLogAppApplication.java
+++ src/main/java/com/rushi/SbLogAppApplication.java
@@ -8,6 +8,11 @@ public class SbLogAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbLogAppApplication.class, args);
+		int add=123;
+		long l=9545127391l;
+		String name="rushi";
+		Double fees=8700.00;
+		
 		
 	}
 
