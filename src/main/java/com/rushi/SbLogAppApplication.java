c14cc4c507b59e7b25737fa65b215e3494e215d4
rushikesh052 <rushikeshshelke052@gmail.com>
2024-03-15 11:42:37 +0530

    app modified

--- src/main/java/com/rushi/SbLogAppApplication.java
+++ src/main/java/com/rushi/SbLogAppApplication.java
@@ -6,12 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbLogAppApplication {
 	
-<<<<<<< HEAD
-=======
 
-	String name="rushi";
-
->>>>>>> 3df27a183990c2fc559577a67b72497a3ab06507
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbLogAppApplication.class, args);	
