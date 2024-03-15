92c3da61b09c643ee8373b0bb97957296a87e08c
rushikesh052 <rushikeshshelke052@gmail.com>
2024-03-15 10:16:30 +0530

    var removed

--- src/main/java/com/rushi/SbLogAppApplication.java
+++ src/main/java/com/rushi/SbLogAppApplication.java
@@ -6,13 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbLogAppApplication {
 	
-	String name="rushi";
 	
-<<<<<<< Updated upstream
 	
-	static String clg="SAC&SCKannad";
-=======
->>>>>>> Stashed changes
+
 	public static void main(String[] args) {
 		SpringApplication.run(SbLogAppApplication.class, args);	
 		
