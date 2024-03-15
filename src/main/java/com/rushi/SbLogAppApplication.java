ad098f75eabd9dba636c8747e840bbb941d44c78
rushikesh052 <rushikeshshelke052@gmail.com>
2024-03-15 10:00:09 +0530

    application file modified

--- src/main/java/com/rushi/SbLogAppApplication.java
+++ src/main/java/com/rushi/SbLogAppApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbLogAppApplication {
+	
+	public void add() {
+		System.out.println("work going on ......");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbLogAppApplication.class, args);	
-		int a=10;
-
-		int b=25;
+		
 
 	}
+	
+	
 
 }
