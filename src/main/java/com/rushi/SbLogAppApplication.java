7a8c2f2da1505733b7041b5e816ab2a80506af6e
rushikesh052 <rushikeshshelke052@gmail.com>
2024-03-15 10:01:02 +0530

    method removed

--- src/main/java/com/rushi/SbLogAppApplication.java
+++ src/main/java/com/rushi/SbLogAppApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbLogAppApplication {
 	
-	public void add() {
-		System.out.println("work going on ......");
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbLogAppApplication.class, args);	
