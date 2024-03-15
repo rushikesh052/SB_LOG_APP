e432c6d7b5869d4cdeab08560b339464dada8b65
rushikesh052 <rushikeshshelke052@gmail.com>
2024-03-15 10:12:42 +0530

    static var added

--- src/main/java/com/rushi/SbLogAppApplication.java
+++ src/main/java/com/rushi/SbLogAppApplication.java
@@ -6,9 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbLogAppApplication {
 	
+	String name="rushi";
 	
+<<<<<<< Updated upstream
 	
 	static String clg="SAC&SCKannad";
+=======
+>>>>>>> Stashed changes
 	public static void main(String[] args) {
 		SpringApplication.run(SbLogAppApplication.class, args);	
 		
