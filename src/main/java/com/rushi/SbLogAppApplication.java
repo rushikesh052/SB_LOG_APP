dc16c7f79ea9496674969b4ec8777f0e56c34f9e
rushikesh052 <148374148+rushikesh052@users.noreply.github.com>
2024-03-15 09:23:20 +0530

    Update SbLogAppApplication.java

--- src/main/java/com/rushi/SbLogAppApplication.java
+++ src/main/java/com/rushi/SbLogAppApplication.java
@@ -7,20 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbLogAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbLogAppApplication.class, args);
-<<<<<<< HEAD
-		int add=123;
-		long l=9545127391l;
-		String name="rushi";
-		Double fees=8700.00;
-		
-=======
-		int id=124;
-		Long no=889009304l;
-		String name="ashok";
-		Double fees=12000.00;
->>>>>>> e11093fa772653b218274eeceba05855a30a39cb
-		
+		SpringApplication.run(SbLogAppApplication.class, args);	
+		int a=10;
 	}
 
 }
