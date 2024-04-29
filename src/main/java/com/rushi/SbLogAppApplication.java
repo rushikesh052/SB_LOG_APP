db03d18741ef407bb1f0fdbf1d68b40735b0b021
rushikesh052 <148374148+rushikesh052@users.noreply.github.com>
2024-04-29 22:18:05 +0530

    Added comments for understanding

--- src/main/java/com/rushi/SbLogAppApplication.java
+++ src/main/java/com/rushi/SbLogAppApplication.java
@@ -5,12 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbLogAppApplication {
-	
 
-
+	//Main Method Started 
 	public static void main(String[] args) {
 		SpringApplication.run(SbLogAppApplication.class, args);	
-		
-
+		System.out.println("Started....");
 	}
+	//Ended
 }
