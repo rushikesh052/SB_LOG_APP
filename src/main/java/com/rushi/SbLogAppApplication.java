6d400fa60f4e655248d532dd88cf510221f9d88a
rushikesh052 <148374148+rushikesh052@users.noreply.github.com>
2024-04-29 22:13:11 +0530

    Removed unwanted spaces from file
    Removed unwanted spaces from file

--- src/main/java/com/rushi/SbLogAppApplication.java
+++ src/main/java/com/rushi/SbLogAppApplication.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbLogAppApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SbLogAppApplication.class, args);	
-		
-
 	}
 
 }
